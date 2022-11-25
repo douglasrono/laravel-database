1e6e9895a920d7892f7dc19ebecd424fdd300a36
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-25 09:56:00 +0300

    Create facade-selectstm.php

---
+++ facade-selectstm.php
@@ -0,0 +1,21 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+ 
+class UserController extends Controller
+{
+    /**
+     * Show a list of all of the application's users.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = DB::select('select * from users where active = ?', [1]);
+ 
+        return view('user.index', ['users' => $users]);
+    }
+}
