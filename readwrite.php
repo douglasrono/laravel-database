92e4cf828b922605778cd28c49d606a7eca2f6a3
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-25 10:59:43 +0300

    Create readwrite.php

---
+++ readwrite.php
@@ -0,0 +1,32 @@
+//Read & Write Connections
+
+'mysql' => [
+    'read' => [
+        'host' => [
+            '192.168.1.1',
+            '196.168.1.2',
+        ],
+    ],
+    'write' => [
+        'host' => [
+            '196.168.1.3',
+        ],
+    ],
+    'sticky' => true,
+    'driver' => 'mysql',
+    'database' => 'database',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+],
+
+/*
+Sometimes you may wish to use one database connection for SELECT statements, and another for INSERT, UPDATE, and DELETE statements. 
+Laravel makes this a breeze, and the proper connections will always be used whether you are using raw queries, the query builder,
+or the Eloquent ORM.
+
+To see how read / write connections should be configured.
+
+*/
