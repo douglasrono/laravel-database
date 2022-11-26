3eede1c6b7b2031e94f3e1477d535a46b0ecfefb
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-26 12:42:34 +0300

    Create havingraw.php

---
+++ havingraw.php
@@ -0,0 +1,5 @@
+$orders = DB::table('orders')
+                ->select('department', DB::raw('SUM(price) as total_sales'))
+                ->groupBy('department')
+                ->havingRaw('SUM(price) > ?', [2500])
+                ->get();
